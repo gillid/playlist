249d6dc03f017dfd83cbf0bfcb4afe6564d39b02
Max Burov <max.gillid@gmail.com>
2025-10-24 11:51:51 +0200

    Install prisma accelerate for connection pooling

--- src/libs/prisma/index.ts
+++ src/libs/prisma/index.ts
@@ -1,16 +1,27 @@
 import { PrismaClient } from '@generated/prisma/client';
+import type { LogLevel } from '@generated/prisma/internal/prismaNamespace';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
-const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+const createPrismaClient = () => {
+  const logLevel: LogLevel[] =
+    process.env.NODE_ENV === 'development'
+      ? ['query', 'error', 'warn']
+      : ['error'];
+
+  const prismaClient = new PrismaClient({
+    log: logLevel,
+  }).$extends(withAccelerate());
+
+  if (prismaClient && process.env.NODE_ENV !== 'production') {
+    globalForPrisma.prisma = prismaClient;
+  }
+
+  return prismaClient;
 };
 
-export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log:
-      process.env.NODE_ENV === 'development'
-        ? ['query', 'error', 'warn']
-        : ['error'],
-  });
+// ensure only one instance of Prisma, even during hot-reloading in development.
+const globalForPrisma = globalThis as unknown as {
+  prisma: ReturnType<typeof createPrismaClient> | undefined;
+};
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
