83faa4eef7d596494e9f69dcbeaecad11caa97b1
Max Burov <max.gillid@gmail.com>
2025-11-02 14:10:03 +0100

    Upgrade to Next.js 16

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,17 @@
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig, globalIgnores } from 'eslint/config';
+import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import nextTypescript from 'eslint-config-next/typescript';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends('next/core-web-vitals', 'next/typescript'),
-  {
-    ignores: [
-      'node_modules/**',
-      '.next/**',
-      'out/**',
-      'build/**',
-      'next-env.d.ts',
-    ],
-  },
-
+const eslintConfig = defineConfig([
+  ...nextCoreWebVitals,
+  ...nextTypescript,
+  globalIgnores([
+    'node_modules/**',
+    '.next/**',
+    'out/**',
+    'build/**',
+    'next-env.d.ts',
+  ]),
   {
     files: ['src/**/*.{ts,tsx,js,jsx}'],
     rules: {
@@ -37,7 +28,6 @@ const eslintConfig = [
       ],
     },
   },
-
   {
     files: ['src/libs/**/*.{ts,tsx,js,jsx}'],
     rules: {
@@ -59,7 +49,6 @@ const eslintConfig = [
       ],
     },
   },
-
   {
     files: [
       'src/libs/_assembly/**',
@@ -79,6 +68,6 @@ const eslintConfig = [
       ],
     },
   },
-];
+]);
 
 export default eslintConfig;
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ['pino'],
+  reactCompiler: true,
   images: {
     remotePatterns: [
       {
