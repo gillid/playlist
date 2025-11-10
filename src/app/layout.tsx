c1aecdb4b971445cfe4acfcfae21c9ccff042855
Max Burov <max.gillid@gmail.com>
2025-11-10 13:47:44 +0100

    Layout, metadata, og:image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,1 +1,51 @@
-export { RootLayout as default } from './_layout/RootLayout';
+import React from 'react';
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import Link from 'next/link';
+import './_layout/globals.css';
+import { Header } from './_layout/Header';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata: Metadata = {
+  title: 'Playlist',
+  description: 'What do you play next?',
+};
+
+const RootLayout = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <html lang='en' className='dark' suppressHydrationWarning>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-dvh flex flex-col`}
+      >
+        <Header />
+        <main className='flex-1 flex flex-col bg-background text-foreground'>
+          {children}
+        </main>
+        <footer className='flex-shrink-0 bg-gradient-to-b from-background to-gray-950 text-foreground text-center py-2'>
+          <Link
+            href='/about'
+            prefetch={false}
+            className='text-sm text-muted-foreground hover:underline'
+          >
+            About
+          </Link>
+        </footer>
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
